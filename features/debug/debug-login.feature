db28b8449a92f06eded3f52877cd431fb6254ae9
David <david.acu02@gmail.com>
2020-05-29 18:59:49 -0600

    cleaning project, delete google examples features...

--- features/debug/debug-login.feature
+++
@@ -1,7 +0,0 @@
-Feature: Debug Login
-
-    Login by API
-
-    @conduit
-    Scenario: should login by API
-        Given that James has already logged in to conduit
--- step-definitions/browser-navigation.js
+++ step-definitions/browser-navigation.js
@@ -4,9 +4,6 @@ const homePage = client.page.home();
 
 Given(/I navigate to (.*) page/, async (siteId) => {
   switch (siteId.toLowerCase()) {
-    case "google home":
-      let google = client.page.google();
-      return await google.navigate();
     case "conduit home":
       return await homePage.navigate().assert.title("Conduit");
   }
